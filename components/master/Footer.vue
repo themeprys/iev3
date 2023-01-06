1541775a58b536d06bf1286e16be6098090e8929
ruhlelana <ruhlelana@gmail.com>
2023-01-06 18:45:57 +0700

    iev3

--- components/master/Footer.vue
+++ components/master/Footer.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
-      Footer
+     <div>
+      
+      <div id="ie_footer footer" class="bg-footer">
+        <div class="row py-4 px-2">
+          <div class="col-3">
+            <img src="https://indonesiaexpat.id/wp-content/uploads/2019/02/desktoplogo1.png" />
+          </div>
+          <div class="col-6 text-light">
+            <ul class="list-unstyled">
+              <li>ABOUT US</li>
+              <li>Indonesia Expat is Indonesia's largest expatriate readership published in Jakarta by PT Koleksi Klasik Indonesia</li>
+              <li>Contact us: info@indonesiaexpat.biz</li>
+            </ul>
+          </div>
+          <div class="col-3 text-light">
+            <ul>FOLLOW US</ul>
+            <button type="button" class="btn btn-primary fa fa-facebook-f"></button>
+            <button type="button" class="btn btn-info fa fa-twitter"></button>
+            <button type="button" class="btn btn-danger fa fa-instagram"></button>
+            <button type="button" class="btn btn-info fa fa-linkedin"></button>
+            <button type="button" class="btn btn-danger fa fa-youtube"></button>
+          </div>
+        </div>
+      </div>
     </div>
   </template>
   
