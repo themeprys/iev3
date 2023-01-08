29365691b28c4bea210b5bf4bdc9f65d83d34869
ruhlelana <ruhlelana@gmail.com>
2023-01-09 00:29:44 +0700

    pembuatan main bottom

--- components/home/Mainbottomleft.vue
+++ components/home/Mainbottomleft.vue
@@ -1,10 +1,58 @@
 <template>
-    <div>
+    <div id="ie_mainbottomleft">
       <div class="card" style="width: 18rem;">
         <h3>Latest Content</h3>
         <div class="row">
-          <div class="col-5">image</div>
-          <div class="col-7">judul</div>
+          <div class="col-5">
+            <img src="https://indonesiaexpat.id/wp-content/uploads/2023/01/hainan-airline.jpg" width="150" height="108" />
+          </div>
+          <div class="col-7 title">
+            <ul class="judul">
+              <li class="judul-item">
+                <a>Indonesia Welcomes Chinese Tourists without Restrictions amidst Spiking Cases</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-5">
+            <img src="https://indonesiaexpat.id/wp-content/uploads/2023/01/diamond-beach-nusa-penida-960x576.jpg" width="150" height="108" />
+          </div>
+          <div class="col-7 title">
+            <ul class="judul">
+              <li class="judul-item">
+                <a>Two Foreigners Dragged into Sea at Diamond Beach Search Ongoing</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-5">
+            <img src="https://indonesiaexpat.id/wp-content/uploads/2023/01/Peacock-CNY-landscape-edited-1.jpg" width="150" height="108" />
+          </div>
+          <div class="col-7 title">
+            <ul class="judul">
+              <li class="judul-item">
+                <a>Celebrate Chinese New Year with Festive Delights at Fairmont Jakarta</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-5">
+            <img src="https://indonesiaexpat.id/wp-content/uploads/2023/01/320236383_1828427577525256_4902612071991048841_n-960x960.jpg" width="150" height="108" />
+          </div>
+          <div class="col-7 title">
+            <ul class="judul">
+              <li class="judul-item">
+                <a>Indonesia Expat Mixer at Gran Melia Jakarta</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-5">
+            <img src="https://indonesiaexpat.id/wp-content/uploads/2023/01/master-960x636.jpg" width="150" height="108" />
+          </div>
+          <div class="col-7 title">
+            <ul class="judul">
+              <li class="judul-item">
+                <a>Is A Master’s Degree Worth the Money?</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- components/home/Mainbottomright.vue
+++ components/home/Mainbottomright.vue
@@ -2,30 +2,38 @@
 <div class="container text-center">
   <div class="row">
     <div class="col">
-      Column
+      <img src="https://indonesiaexpat.id/wp-content/uploads/2023/01/320236383_1828427577525256_4902612071991048841_n-960x960.jpg" width="225" height="150" />
+      <h6>Indonesia Expat Mixer at Gran Melia Jakarta</h6>
     </div>
     <div class="col">
-      Column
+      <img src="https://indonesiaexpat.id/wp-content/uploads/2022/11/indonesia-expat-mixer-bali-960x642.jpg" width="225" height="150" />
+      <h6>Indonesia Expat Mixer at Hotel Indigo Seminyak Bali</h6>
     </div>
     <div class="col">
-      Column
+      <img src="https://indonesiaexpat.id/wp-content/uploads/2022/11/Poolside-Indonesia-Expat-Mixer-at-Pastis-Pool-and-Bar.jpeg" width="225" height="150" />
+      <h6>Poolside Indonesia Expat Mixer at Pastis Pool and Bar</h6>
     </div>
     <div class="col">
-      Column
+      <img src="https://indonesiaexpat.id/wp-content/uploads/2022/11/INDONESIA-EXPAT-MIXER-RETURNS-TO-THE-CAPITAL.jpeg" width="225" height="150" />
+      <h6>INDONESIA EXPAT MIXER RETURNS TO THE CAPITAL</h6>
     </div>
   </div>
   <div class="row">
     <div class="col">
-      Column
+      <img src="https://indonesiaexpat.id/wp-content/uploads/2022/11/Hard-Rock-Cafe-Jakarta.jpeg" width="225" height="150" />
+      <h6>Hard Rock Cafe Jakarta</h6>
     </div>
     <div class="col">
-      Column
+      <img src="https://indonesiaexpat.id/wp-content/uploads/2022/07/business-mixer-July-2022.jpg" width="225" height="150" />
+      <h6>Business Mixer – Updates on Visa Regulation and Investment Promotion from BPKM</h6>
     </div>
     <div class="col">
-      Column
+      <img src="https://indonesiaexpat.id/wp-content/uploads/2022/07/Indonesia-Expat-Mixer-June-2022.jpeg" width="225" height="150" />
+      <h6>A Night of Music – Indonesia Expat Mixer</h6>
     </div>
     <div class="col">
-      Column
+      <img src="https://indonesiaexpat.id/wp-content/uploads/2022/05/Youtube-Beer-Video-e1653800085147.png" width="225" height="150" />
+      <h6>Must Try Indonesian Beers</h6>
     </div>
   </div>
 </div>
